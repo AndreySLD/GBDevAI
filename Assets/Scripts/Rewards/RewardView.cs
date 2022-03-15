d3cfcf0b8eb490af533b52da27eec2fd52d21ff5
Andrey <gitlerkaputs@yandex.ru>
2022-03-15 18:50:45 +0300

    HW8

---
+++ Assets/Scripts/Rewards/RewardView.cs
@@ -0,0 +1,160 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RewardView : MonoBehaviour
+{
+    private const string LastTimeDailyKey = "LastDailyRewardTime";
+    private const string ActiveDailySlotKey = "ActiveDailySlot";
+
+    private const string LastTimeWeeklyKey = "LastWeekRewardTime";
+    private const string ActiveWeeklySlotKey = "ActiveWeeklySlot";
+
+    #region Fields
+    [Header("Time settings")]
+    [SerializeField]
+    public int DailyTimeCooldown = 86400;
+    [SerializeField]
+    public int DailyTimeDeadline = 172800;
+    [SerializeField]
+    public int WeeklyTimeCooldown;
+    [SerializeField]
+    public int WeeklyTimeDeadline;
+    [Space]
+    [Header("Reward Settings")]
+    public List<Reward> DailyRewards;
+    public List<Reward> WeeklyRewards;
+    [Header("UI")]
+    [SerializeField]
+    public TMP_Text DailyRewardTimer;
+    [SerializeField]
+    public TMP_Text WeeklyRewardTimer;
+    [SerializeField]
+    public Transform SlotsParent;
+    [SerializeField]
+    public SlotRewardView SlotPrefab;
+    [SerializeField]
+    public Button ResetButton;
+    [SerializeField]
+    public Button CloseButton;
+    [SerializeField]
+    public Button GetDailyRewardButton;
+    [SerializeField]
+    public Button GetWeeklyRewardButton;
+    [SerializeField]
+    public Slider ProgressBarDaily;
+    [SerializeField]
+    public Slider ProgressBarWeekly;
+    [Header("AnimSettings")]
+    [SerializeField]
+    private float _duration = 0.3f;
+
+    private DailyRewardController _dailyRewardController;
+    private WeeklyRewardController _weeklyRewardController;
+
+    private bool _state;
+    #endregion
+
+    public int CurrentActiveDailySlot
+    {
+        get => PlayerPrefs.GetInt(ActiveDailySlotKey);
+        set => PlayerPrefs.SetInt(ActiveDailySlotKey, value);
+    }
+
+    public int CurrentActiveWeeklySlot
+    {
+        get => PlayerPrefs.GetInt(ActiveWeeklySlotKey);
+        set => PlayerPrefs.GetInt(ActiveWeeklySlotKey, value);
+    }
+
+    public DateTime? LastDailyRewardTime
+    {
+        get
+        {
+            var data = PlayerPrefs.GetString(LastTimeDailyKey);
+            if (string.IsNullOrEmpty(data))
+                return null;
+            return DateTime.Parse(data);
+        }
+        set
+        {
+            if (value != null)
+                PlayerPrefs.SetString(LastTimeDailyKey, value.ToString());
+            else
+                PlayerPrefs.DeleteKey(LastTimeDailyKey);
+        }
+    }
+
+    public DateTime? LastWeeklyRewardTime
+    {
+        get
+        {
+            var data = PlayerPrefs.GetString(LastTimeWeeklyKey);
+            if (string.IsNullOrEmpty(data))
+                return null;
+            return DateTime.Parse(data);
+        }
+        set
+        {
+            if (value != null)
+                PlayerPrefs.SetString(LastTimeWeeklyKey, value.ToString());
+            else
+                PlayerPrefs.DeleteKey(LastTimeWeeklyKey);
+        }
+    }
+
+    #region Animation
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        _state = true;
+        Animation(_state, _duration);
+    }
+    public void Hide()
+    {
+        _state = false;
+        Animation(_state, _duration);
+    }
+    private void Animation(bool state, float value)
+    {
+        var sequence = DOTween.Sequence();
+
+        if (state)
+        {
+            sequence.Insert(0.0f, transform.DOScale(Vector3.one, value));
+            sequence.OnComplete(() =>
+            {
+                sequence = null;
+            });
+        }
+        else
+        {
+            sequence.Insert(0.0f, transform.DOScale(Vector3.zero, value));
+            sequence.OnComplete(() =>
+            {
+                sequence = null;
+                gameObject.SetActive(false);
+            });
+
+        }
+    }
+    #endregion
+    private void Start()
+    {
+        _dailyRewardController = new DailyRewardController(this);
+        _weeklyRewardController = new WeeklyRewardController(this);
+
+        CloseButton.onClick.AddListener(Hide);
+    }
+    private void OnDestroy()
+    {
+        GetDailyRewardButton.onClick.RemoveAllListeners();
+        GetWeeklyRewardButton.onClick.RemoveAllListeners();
+        ResetButton.onClick.RemoveAllListeners();
+    }
+}
---
+++ Assets/Scripts/Rewards/WeeklyRewardController.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeeklyRewardController : IDisposable
+{
+    private readonly RewardView _rewardView;
+    private List<SlotRewardView> _slots;
+
+    private bool _rewardReceived = false;
+
+    public WeeklyRewardController(RewardView rewardView)
+    {
+        _rewardView = rewardView;
+        InitSlots();
+        RefreshUi();
+        _rewardView.StartCoroutine(UpdateCoroutine());
+        SubscribeButtons();
+    }
+
+    private IEnumerator UpdateCoroutine()
+    {
+        while (true)
+        {
+            Update();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    private void Update()
+    {
+        RefreshRewardState();
+        RefreshUi();
+    }
+
+    private void RefreshRewardState()
+    {
+        _rewardReceived = false;
+        if (_rewardView.LastWeeklyRewardTime.HasValue)
+        {
+            var timeSpan = DateTime.UtcNow - _rewardView.LastWeeklyRewardTime.Value;
+            if (timeSpan.Seconds > _rewardView.WeeklyTimeDeadline)
+            {
+                _rewardView.LastWeeklyRewardTime = null;
+                _rewardView.CurrentActiveWeeklySlot = 0;
+            }
+            else if (timeSpan.Seconds < _rewardView.WeeklyTimeCooldown)
+            {
+                _rewardReceived = true;
+            }
+        }
+    }
+
+    private void RefreshUi()
+    {
+
+        _rewardView.GetWeeklyRewardButton.interactable = !_rewardReceived;
+
+        for (var i = 0; i < _rewardView.WeeklyRewards.Count; i++)
+        {
+            _slots[i].SetData(_rewardView.WeeklyRewards[i], i + 1, i <= _rewardView.CurrentActiveWeeklySlot);
+        }
+
+        DateTime nextWeekBonusTime =
+            !_rewardView.LastWeeklyRewardTime.HasValue
+                ? DateTime.MinValue
+                : _rewardView.LastWeeklyRewardTime.Value.AddSeconds(_rewardView.WeeklyTimeCooldown);
+        var delta = nextWeekBonusTime - DateTime.UtcNow;
+        if (delta.TotalSeconds < 0)
+            delta = new TimeSpan(0);
+
+        _rewardView.WeeklyRewardTimer.text = delta.ToString();
+        _rewardView.ProgressBarWeekly.value = (float)delta.TotalSeconds;
+    }
+
+    private void InitSlots()
+    {
+        _slots = new List<SlotRewardView>();
+        for (int i = 0; i < _rewardView.WeeklyRewards.Count; i++)
+        {
+            var reward = _rewardView.WeeklyRewards[i];
+            var slotInstance = GameObject.Instantiate(_rewardView.SlotPrefab, _rewardView.SlotsParent, false);
+            slotInstance.SetData(reward, i + 1, false);
+            _slots.Add(slotInstance);
+        }
+
+        _rewardView.ProgressBarWeekly.maxValue = _rewardView.WeeklyTimeCooldown;
+    }
+
+    private void SubscribeButtons()
+    {
+        _rewardView.ResetButton.onClick.AddListener(ResetReward);
+        _rewardView.GetWeeklyRewardButton.onClick.AddListener(ClaimReward);
+    }
+
+    private void ResetReward()
+    {
+        _rewardView.LastWeeklyRewardTime = null;
+        _rewardView.CurrentActiveWeeklySlot = 0;
+    }
+
+    private void ClaimReward()
+    {
+        if (_rewardReceived)
+        {
+            return;
+        }
+
+        var reward = _rewardView.WeeklyRewards[_rewardView.CurrentActiveWeeklySlot];
+        switch (reward.TypeByCur)
+        {
+            case RewardTypeByCurrency.None:
+                break;
+            case RewardTypeByCurrency.Wood:
+                CurrencyWindow.Instance.AddWood(reward.Count);
+                break;
+            case RewardTypeByCurrency.Diamond:
+                CurrencyWindow.Instance.AddDiamond(reward.Count);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        _rewardView.LastWeeklyRewardTime = DateTime.UtcNow;
+        _rewardView.CurrentActiveWeeklySlot = (_rewardView.CurrentActiveWeeklySlot + 1) % _rewardView.WeeklyRewards.Count;
+        RefreshRewardState();
+    }
+    public void Dispose()
+    {
+        
+    }
+}
